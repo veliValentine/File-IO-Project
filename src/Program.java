e72948e6ff6969c96b8bc5df0ba46f9696ca1514
nicolas.valentine <nikkevanukas1995@gmail.com>
2021-01-21 17:46:32 +0200

    feat: fileManipulation in main-loop The main loop now handles the fileManipulation cases Also refactored action prints to own methods

--- src/Program.java
+++ src/Program.java
@@ -5,51 +5,95 @@ import interactions.UserInteractions;
 
 public class Program {
     public static void main(String[] args) {
-        program();
-    }
-
-    public static void program() {
-
         Files files = new Files();
         UserInteractions interact = new UserInteractions();
 
-        System.out.println("\nWelcome to File IO");
+        System.out.println("-------------------");
+        System.out.println("Welcome to the File IO");
         while (true) {
             System.out.println("-------------------");
-            System.out.println("Type code for action");
-            System.out.println("  1: List all files");
-            System.out.println("  2: Get files by extension");
-            System.out.println("  3: Manipulate file");
-            System.out.println("  0: exit");
+            printMainLoopActions();
             int userInput = interact.inputInt();
+            System.out.println();
             if (userInput == 0) {
                 break;
+            } else if (userInput == 1) {
+                System.out.println("All files");
+                files.printAllFileNames();
+
+            } else if (userInput == 2) {
+                System.out.println("Extensions");
+                Set<String> allExtensions = files.listAllFileExtensions();
+                System.out.println();
+                String extension = interact.inputExtension(allExtensions);
+                System.out.println();
+                files.printAllFileNames(extension);
+
+            } else if (userInput == 3) {
+                Set<String> allTextFiles = files.listAllUniqueFileNames("txt");
+                String fileName = interact.inputFile(allTextFiles);
+                System.out.println();
+                if (!fileName.equals("")) { // else continue main-loop
+                    while (true) {
+                        printFileManipulationActions();
+                        int action = interact.inputInt();
+                        System.out.println();
+                        if (action == 0) {
+                            break;
+
+                        } else if (action == 1) {
+                            System.out.println("  " + fileName);
+
+                        } else if (action == 2) {
+                            long size = files.size(fileName);
+                            System.out.println("  " + size + " bytes or " + (size / 1024.0) + " kilobytes");
+
+                        } else if (action == 3) {
+                            long lines = files.amountOfLines(fileName);
+                            System.out.println("  " + lines + " lines");
+
+                        } else if (action == 4) {
+                            String word = interact.input("Enter word: ");
+                            System.out.println();
+                            if (files.containsWord(word, fileName)) {
+                                System.out.println("  File contains the word: " + word + "!");
+                            } else {
+                                System.out.println("  File doesn't contain the word: " + word);
+                            }
+
+                        } else if (action == 5) {
+                            String word = interact.input("Enter word: ");
+                            System.out.println();
+                            long count = files.countWord(word, fileName);
+                            System.out.println("The word " + word + " was " + count + " times in the file " + fileName);
+                        }
+                        interact.input("Press enter to continue");
+                    }
+                }
             }
-            switch (userInput) {
-                case 1:
-                    System.out.println("All files");
-                    files.listAllFileNames();
-                    break;
-                case 2:
-                    System.out.println("Extensions");
-                    Set<String> allExtensions = files.listAllFileExtensions();
-                    System.out.println("Choose one...");
-                    String extension = interact.inputExtension(allExtensions);
-                    files.listAllFileNames(extension);
-                    break;
-                case 3:
-                    System.out.println("Asks file name");
-                    System.out.println("List all possible methods");
-                    break;
-                default:
-                    System.out.println("Command not listed");
-                    break;
-            }
-            System.out.println("Press enter to continue");
-            interact.input();
+
+            interact.input("Press enter to continue");
         }
         interact.close();
         System.out.println("Thank you for using File IO");
+    }
+
+    private static void printMainLoopActions() {
+        System.out.println("Codes for actions");
+        System.out.println("  1: List all files");
+        System.out.println("  2: Get files by extension");
+        System.out.println("  3: Manipulate .txt file");
+        System.out.println("  0: exit");
+    }
+
+    private static void printFileManipulationActions() {
+        System.out.println("Choose action");
+        System.out.println("  1:Print file name");
+        System.out.println("  2:Print file size");
+        System.out.println("  3:Print amount of lines");
+        System.out.println("  4:See if specific word exists");
+        System.out.println("  5:Count how many times a word is in the file");
+        System.out.println("  0: Exit");
     }
 
 }
