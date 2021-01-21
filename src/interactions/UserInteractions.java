92e8382c38cfe62545970aab589e3b338c460a4c
nicolas.valentine <nikkevanukas1995@gmail.com>
2021-01-21 17:41:09 +0200

    feat: inputFile & input(String) inputFile reads users input for file name. Added input(String) for output message

--- src/interactions/UserInteractions.java
+++ src/interactions/UserInteractions.java
@@ -16,12 +16,18 @@ public class UserInteractions {
     }
 
     public String input() {
-        String input = scanner.nextLine().trim().toLowerCase();
+        return input("");
+    }
+
+    public String input(String text) {
+        System.out.print(text);
+        String input = scanner.nextLine().trim();
         return input;
     }
 
     public int inputInt() {
         while (true) {
+            System.out.print("Enter integer: ");
             String input = input();
             try {
                 return Integer.parseInt(input);
@@ -34,11 +40,24 @@ public class UserInteractions {
     public String inputExtension(Set<String> extensions) {
         System.out.println("To quit press enter");
         while (true) {
+            System.out.print("Enter extension: ");
             String extension = input();
             if (extensions.contains(extension) || extension.equals("")) {
                 return extension;
             }
             System.out.println("Not valid extension");
+        }
+    }
+
+    public String inputFile(Set<String> fileNames) {
+        System.out.println("Press enter to quit");
+        while (true) {
+            System.out.print("Enter file name: ");
+            String file = input();
+            if (file.equals("") || fileNames.contains(file)) {
+                return file;
+            }
+            System.out.println("File not found");
         }
     }
 }
